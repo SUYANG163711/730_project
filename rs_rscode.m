0641a25ce7123334233879782026d441b1498d25
李孝阳 <161676998+SUYANG163711@users.noreply.github.com>
2024-11-23 20:33:37 +0800

    wangyong V3

--- rs_rscode.m
+++ rs_rscode.m
@@ -1,8 +1,4 @@
-% RS������ӳ���
-function code = rs_rscode(msg)
-    g = [1 0 0 3 2 1 3;
-         0 1 0 5 5 1 4;
-         0 0 1 7 6 1 6];
-    g = gf(g, 3);
-    code = msg * g;
+function code = rs_rscode(msg, g)
+    % 使用生成矩阵对消息进行 RS 编码
+    code = msg * g; % 矩阵乘法，生成码字
 end
